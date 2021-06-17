7d928dd88507c0fc1d99ea913fc7ed6516f6e121
tamas.siklosi <tamas.siklosi@accenture.com>
2021-06-17 19:37:17 +0200

    save on exit

--- src/main/java/org/siki/cashcounter/CashCounter.java
+++ src/main/java/org/siki/cashcounter/CashCounter.java
@@ -2,10 +2,14 @@ package org.siki.cashcounter;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.siki.cashcounter.repository.DataManager;
 import org.siki.cashcounter.view.MainScene;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.IOException;
 
 @Slf4j
 public class CashCounter extends Application {
@@ -20,6 +24,19 @@ public class CashCounter extends Application {
     context = new AnnotationConfigApplicationContext(Config.class);
     var mainScene = context.getBean(MainScene.class);
     stage.setScene(mainScene);
+    setCloseBehaviour(stage);
     stage.show();
+  }
+
+  private void setCloseBehaviour(Stage stage) {
+    stage.setOnCloseRequest(
+        (WindowEvent event) -> {
+          var dataManager = context.getBean(DataManager.class);
+          try {
+            dataManager.save();
+          } catch (IOException ex) {
+            log.error("Error while save on closing", ex);
+          }
+        });
   }
 }
